5c039c5b1d7fb6a0fc43e5b46e82cd815f74bc18
Saman Attar <saman@Samans-MBP.home>
2021-09-16 14:33:47 -0500

    Updating book links

--- src/pages/books.js
+++ src/pages/books.js
@@ -7,23 +7,23 @@ const IndexPage = () => {
       <Layout className={background}>
         <h1>Engineering</h1>
         <ul>
-            <li><a target="_blank" href="https://www.okta.com/">Clean Code</a> - Uncle Bob </li>
-            <li><a target="_blank" href="https://www.okta.com/">Clean Architecture</a> - Uncle Bob </li>
-            <li><a target="_blank" href="https://www.okta.com/">Ask Your Developer</a> - Jeff Lawson </li>
+            <li><a target="_blank" href="https://www.goodreads.com/book/show/3735293-clean-code">Clean Code</a> - Uncle Bob </li>
+            <li><a target="_blank" href="https://www.goodreads.com/book/show/18043011-clean-architecture">Clean Architecture</a> - Uncle Bob </li>
+            <li><a target="_blank" href="https://www.goodreads.com/book/show/48716700-ask-your-developer">Ask Your Developer</a> - Jeff Lawson </li>
         </ul>
 
         <h1>Psychology & Sociology</h1>
         <ul>
-            <li><a target="_blank" href="https://www.okta.com/">Thinking, Fast and Slow</a> - Daniel Kahneman</li>
-            <li><a target="_blank" href="https://www.okta.com/">The Tipping Point</a> - Malcom Gladwell </li>
-            <li><a target="_blank" href="https://www.okta.com/">Think Again</a> - Adam Grant </li>
+            <li><a target="_blank" href="https://www.goodreads.com/book/show/11468377-thinking-fast-and-slow">Thinking, Fast and Slow</a> - Daniel Kahneman</li>
+            <li><a target="_blank" href="https://www.goodreads.com/book/show/2612.The_Tipping_Point">The Tipping Point</a> - Malcom Gladwell </li>
+            <li><a target="_blank" href="https://www.goodreads.com/book/show/55539565-think-again">Think Again</a> - Adam Grant </li>
         </ul>
 
         <h1>Cooking</h1>
         <ul>
-            <li><a target="_blank" href="https://www.okta.com/">Salt Fat Acid Heat</a> - Samin Nosrat</li>
-            <li><a target="_blank" href="https://www.okta.com/">Flour Water Salt Yeast</a> - Ken Forkish </li>
-            <li><a target="_blank" href="https://www.okta.com/">Food of Life: Ancient Persian and Modern Iranian Cooking</a> - Najmieh Batmanglij </li>
+            <li><a target="_blank" href="https://www.goodreads.com/book/show/30753841-salt-fat-acid-heat">Salt Fat Acid Heat</a> - Samin Nosrat</li>
+            <li><a target="_blank" href="https://www.goodreads.com/book/show/13414492-flour-water-salt-yeast">Flour Water Salt Yeast</a> - Ken Forkish </li>
+            <li><a target="_blank" href="https://www.goodreads.com/book/show/110103.New_Food_of_Life">Food of Life: Ancient Persian and Modern Iranian Cooking</a> - Najmieh Batmanglij </li>
         </ul>
         
       </Layout>
