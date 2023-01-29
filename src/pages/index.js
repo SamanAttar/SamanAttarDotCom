b5815e52811dee149d618aac3285ab8fc14bdc82
Saman Attar <saman.attar@outlook.com>
2023-01-28 18:20:41 -0600

    remove field notes + add campapex

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,58 @@
-import * as React from 'react'
-import Layout from '../components/layout'
-import { background } from './index.module.css'
-import Seo from "../components/seo"
-import "@fontsource/varela-round"; 
-
-
+import * as React from "react";
+import Layout from "../components/layout";
+import { background } from "./index.module.css";
+import Seo from "../components/seo";
+import "@fontsource/varela-round";
 
 const IndexPage = (data) => {
-  const siteTitle = 'Saman Attar';
+  const siteTitle = "Saman Attar";
   return (
-      <Layout className={background} title={siteTitle}>
-        <Seo title="Howdy" />
-            
-        <p>Howdy! I'm <a target="_blank" href="https://twitter.com/SOQL101">Saman</a> (he/him). I cook, bake bread, and build software. </p>
+    <Layout className={background} title={siteTitle}>
+      <Seo title="Howdy" />
 
-        <p>I specialize in <a target="_blank" href="https://trailblazer.me/id/saman">Salesforce</a> and write about my developer experience in the <a href="/field-notes-on-salesforce">Field Notes on Salesforce</a> series. </p>
+      <p>
+        Howdy! I'm{" "}
+        <a target="_blank" href="https://twitter.com/SOQL101" rel="noreferrer">
+          Saman
+        </a>{" "}
+        (he/him). I cook, bake bread, and build software.{" "}
+      </p>
 
-        <p>I'm a Sofware Engineer at Okta. I've also been a Tech Lead at USAA and President of UT Dallas's ACM chapter. Whoosh!</p>
-      
-        <p>For dinner, I prefer to stay in and <a target="_blank" href="https://vsco.co/saman-attar/gallery">cook</a>. I've been trying to read more. These <a href="/books">books</a> have made personal and professional impacts on me.</p> 
-        
-      </Layout>
-  )
-}
+      <p>
+        I specialize in{" "}
+        <a
+          target="_blank"
+          href="https://trailblazer.me/id/saman"
+          rel="noreferrer"
+        >
+          Salesforce.
+        </a>{" "}
+        Checkout my recent project,{" "}
+        <a target="_blank" href="https://www.campapex.org">
+          CampApex.org
+        </a>
+        .
+      </p>
 
-export default IndexPage
+      <p>
+        I'm a Sofware Engineer at Okta. I've also been a Tech Lead at USAA and
+        President of UT Dallas's ACM chapter. Whoosh!
+      </p>
+
+      <p>
+        For dinner, I prefer to stay in and{" "}
+        <a
+          target="_blank"
+          href="https://vsco.co/saman-attar/gallery"
+          rel="noreferrer"
+        >
+          cook
+        </a>
+        . I've been trying to read more. These <a href="/books">books</a> have
+        made personal and professional impacts on me.
+      </p>
+    </Layout>
+  );
+};
+
+export default IndexPage;
